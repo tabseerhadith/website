227296a65acf1e5763ebed6f3066acf1a31832a4
Abdullah Zubayr <abdullahzubayr@proton.me>
2025-10-05 12:08:03 +0530

    misc

--- frontend/src/routes/[collection]/[book]/[hadith]/+page.server.ts
+++ frontend/src/routes/[collection]/[book]/[hadith]/+page.server.ts
@@ -1,25 +1,25 @@
-import collections from "$lib/collections";
-
-export const load = async ({ params }) => {
-  let collection = collections.find((x) => x.name === params.collection);
-  collection = collection?.books || [];
-
-  let data;
-
-  collection.some((book, i) =>
-    book.chapters.some((chapter) =>
-      chapter.hadiths.some((x) => {
-        if (String(x.id) === params.hadith) {
-          data = {
-            hadith: x,
-            chapter: chapter.name,
-            book: { name: book.name, index: i },
-          };
-          return true;
-        }
-      }),
-    ),
-  );
-
-  return data;
-};
+// import collections from "$lib/collections";
+//
+// export const load = async ({ params }) => {
+//   let collection = collections.find((x) => x.name === params.collection);
+//   collection = collection?.books || [];
+//
+//   let data;
+//
+//   collection.some((book, i) =>
+//     book.chapters.some((chapter) =>
+//       chapter.hadiths.some((x) => {
+//         if (String(x.id) === params.hadith) {
+//           data = {
+//             hadith: x,
+//             chapter: chapter.name,
+//             book: { name: book.name, index: i },
+//           };
+//           return true;
+//         }
+//       }),
+//     ),
+//   );
+//
+//   return data;
+// };
--- frontend/src/routes/[collection]/[book]/[hadith]/+page.svelte
+++ frontend/src/routes/[collection]/[book]/[hadith]/+page.svelte
@@ -1,35 +1,37 @@
-<script lang="ts">
-  const { params, data } = $props();
+<!-- <script lang="ts"> -->
+<!--   const { params, data } = $props(); -->
+<!---->
+<!--   const { hadith } = data; -->
+<!-- </script> -->
+<!---->
+<!-- <main class="max-w-screen-lg mx-auto"> -->
+<!--   <section class="text-xl p5 rounded frow"> -->
+<!--     <i class="i-pepicons-pop:book"></i> -->
+<!--     Book Name here -->
+<!--   </section> -->
+<!---->
+<!--   <section class="text-xl brd p5 rounded frow mb5"> -->
+<!--     <i class="i-lets-icons:book"></i> -->
+<!--     {@html data.chapter} -->
+<!--   </section> -->
+<!---->
+<!--   <section> -->
+<!--     <div -->
+<!--       class="grid-(~ cols-[1fr_auto_1fr] gap5) bg-secondary p5 rounded leading-loose" -->
+<!--     > -->
+<!--       <div class="grid gap3 h-fit"> -->
+<!--         {@html hadith.english_text} -->
+<!--       </div> -->
+<!--       <div class="brd"></div> -->
+<!--       <div class="arabic_txt">{@html hadith.arabic_text}</div> -->
+<!--       <button -->
+<!--         aria-labelledby="bookmark" -->
+<!--         class="btn-secondary-eqmd bg-border hover:bg-border/80 rounded-full w-fit" -->
+<!--       > -->
+<!--         <i class="i-mingcute:bookmark-line"></i> -->
+<!--       </button> -->
+<!--     </div> -->
+<!--   </section> -->
+<!-- </main> -->
 
-  const { hadith } = data;
-</script>
-
-<main class="max-w-screen-lg mx-auto">
-  <section class="text-xl p5 rounded frow">
-    <i class="i-pepicons-pop:book"></i>
-    Book Name here
-  </section>
-
-  <section class="text-xl brd p5 rounded frow mb5">
-    <i class="i-lets-icons:book"></i>
-    {@html data.chapter}
-  </section>
-
-  <section>
-    <div
-      class="grid-(~ cols-[1fr_auto_1fr] gap5) bg-secondary p5 rounded leading-loose"
-    >
-      <div class="grid gap3 h-fit">
-        {@html hadith.english_text}
-      </div>
-      <div class="brd"></div>
-      <div class="arabic_txt">{@html hadith.arabic_text}</div>
-      <button
-        aria-labelledby="bookmark"
-        class="btn-secondary-eqmd bg-border hover:bg-border/80 rounded-full w-fit"
-      >
-        <i class="i-mingcute:bookmark-line"></i>
-      </button>
-    </div>
-  </section>
-</main>
+kekw bro
